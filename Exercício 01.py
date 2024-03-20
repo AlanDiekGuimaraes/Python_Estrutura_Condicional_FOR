483f928a0a5492066e328f8888be46fecfc887f3
AlanDiekGuimaraes <126631754+AlanDiekGuimaraes@users.noreply.github.com>
2024-03-19 23:19:29 -0300

    Atualização

---
+++ Exercício 01.py
@@ -0,0 +1,6 @@
+## Python
+#Para o contador de 1 até 4 faça um programa que receba 1 número e multiplique por 12
+for contador in range(1,5):
+  x = float(input("Digite um valor para multiplicar por 12 "))
+  r = x*12
+  print(f"A multiplicação é {r:.0f}")
---
+++ Exercício 02.py
@@ -0,0 +1,10 @@
+# Faça um programa que leia 5 números e informe o maior número.
+
+maior_numero = float('-inf')
+
+for contador in range(5):
+  numero = float(input(f"Digite o {contador+1}º número "))
+  if  numero > maior_numero:
+    maior_numero = numero
+
+  print(f"O maior numero digitado é: {maior_numero}")
